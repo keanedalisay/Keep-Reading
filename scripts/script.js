a9406b4c024c002ad09ef8f908914d2990856252
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-08-23 16:11:57 +0800

    Add objects to store buttons and popups

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const buttons = {
+    addBookBtn : document.querySelector('.add-book-button'),
+    subAddBookBtn : document.querySelector('.add-book'),
+    subChangeBookInfoBtn : document.querySelector('.change-book-info'),
+    closeBtns : document.querySelectorAll('.close-button'),
+    deleteBtn : document.querySelector('.delete-button'),
+}
+
+const popups = {
+    noBookPopup : document.querySelector('.no-book-popup'),
+    addBookPopup : document.querySelector('.add-book-popup'),
+    bookInfoPopup : document.querySelector('.book-info-popup'),
+}
+
